f33b55827e15085e3d0d3c7970bce5a1a292a0ae
Shander Andrade <s.hander8910@hotmail.com>
2022-10-02 20:39:24 -0300

    .

--- myKeys/rev2/keymaps/try/keymap.c
+++ myKeys/rev2/keymaps/try/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------------------------------------------------------------------.
  * |   '   |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |       )       |      ~ `   |
  * |------+------+------+------+------+------+------+------+------+------+---------------+-------------|
- * |      |      |      |   F2   |  F4  | F5  |    |   F12- |   +  |  {   |            }   |      Pipe   |
+ * |      |      |   F2       |  F4  | F5  |  F6  |   F12 |  - |   +  |  {   |     }        |      Pipe |
  * |------+------+------+------+------+------+------+------+------+------+---------------+-------------|
  * |LShift|    |    |    |   |   |   |S(NUHS)|S(NUBS)|Home|       End     |                            |
  * |------+------+------+------+------+------+------+------+------+------+---------------+-------------|
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_ortho_4x12_1x2uC(
 
       KC_QUOT , KC_EXLM , KC_AT   , KC_HASH , KC_DLR , KC_PERC , KC_CIRC , KC_AMPR    , KC_ASTR    , KC_LPRN , KC_RPRN , KC_GRV,
-      KC_NO   , KC_NO   , KC_F2   ,  KC_F4  , KC_F5 ,  KC_F6  , KC_F12   , KC_UNDS    , KC_PLUS    , KC_LCBR , KC_RCBR , KC_PIPE,
+      KC_NO   ,  KC_NO  ,  KC_F2  ,  KC_F4  , KC_F5 ,  KC_F6  , KC_F12   , KC_UNDS    , KC_PLUS    , KC_LCBR , KC_RCBR , KC_PIPE,
       KC_LSFT , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_NO  , KC_NO  , S(KC_NUHS) , S(KC_NUBS) , KC_HOME , KC_END  , KC_NO,
       KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO  , MO(3)   , KC_NO   , KC_NO      , KC_NO      , CEDILHA , AO
 
@@ -78,17 +78,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------------------------------------------------------------------.
  * |      |   1!  |   2@  |   3#  |   4$  |   5%  |   6^  |   7&  |   8*  |   9(  |   0) |  Backspace  |
  * |------+------+------+------+------+------+------+------+------+------+---------------+-------------|
- * |      |  F1  | F2   | F3   | F4   | F5   |  F6  |   -_ |   += |  [{  |  }]           |    \ |      |
+ * |      |    |  F2  |   F4   | F5   |  F6  |   F12    |   -_ |   += |  [{  |  }]           |    \ |  |
  * |------+------+------+------+------+------+------+------+------+------+---------------+-------------|
- * |LShift|  F7  |  F8  |  F9   | F10 | F11  | F12  | NUHS | NUBS | PgUp  |   PgDn       |             |
+ * |LShift|      |      |      |      |      |       | NUHS | NUBS | PgUp  |   PgDn       |            |
  * |------+------+------+------+------+------+------+------+------+------+---------------+-------------|
  * |      |      |      |      |      |             |      |Next  |Vol - |   Vol +       |    Play     |
  * `---------------------------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12_1x2uC(
       KC_NO  , KC_1  , KC_2  , KC_3  , KC_4   , KC_5   , KC_6   , KC_7    , KC_8    , KC_9     ,KC_0    , KC_BSPC,
-      KC_NO , KC_F1 , KC_F2 , KC_F3 , KC_F4  , KC_F5  , KC_F6  , KC_MINS , KC_EQL  , KC_LBRC  ,KC_RBRC , KC_BSLS,
-      KC_LSFT, KC_F7 , KC_F8 , KC_F9 , KC_F10 , KC_F11 , KC_F12 , KC_NUHS , KC_NUBS , KC_PGUP  ,KC_PGDN , KC_NO,
+      KC_NO , KC_NO , KC_F2 , KC_F4 , KC_F5  , KC_F6  , KC_F12  , KC_MINS , KC_EQL  , KC_LBRC  ,KC_RBRC , KC_BSLS,
+      KC_LSFT, KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NUHS , KC_NUBS , KC_PGUP  ,KC_PGDN , KC_NO,
       KC_NO  , KC_NO , KC_NO , KC_NO , KC_NO  , KC_NO  , KC_NO  , KC_MNXT , KC_VOLD , KC_VOLU  ,KC_MPLY
 
 ),
