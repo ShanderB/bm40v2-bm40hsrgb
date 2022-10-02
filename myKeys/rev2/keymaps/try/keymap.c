a6d46f5bacc2719e956c29a0f1831c6113a280cd
Shander Andrade <s.hander8910@hotmail.com>
2022-10-02 11:32:35 -0300

    changes button from layers, del and enter

--- myKeys/rev2/keymaps/try/keymap.c
+++ myKeys/rev2/keymaps/try/keymap.c
@@ -40,27 +40,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------------------------------------------------------------------.
  * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |     Backspace   |
  * |------+------+------+------+------+------+------+------+------+------+------+-----------------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |      Enter      |
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |      Del        |
  * |------+------+------+------+------+------+------+------+------+------+------+-----------------|
- * | LShift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |       Del      |
+ * | LShift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |                |
  * |------+------+------+------+------+------+------+------+------+------+------+-----------------|
- * | LCtrl | GUI |      | LAlt  |        |    Space    |  Lower  | Up | Left |  Down  |   Right   |
+ * | LCtrl | GUI |  Lower    | LAlt  |   Enter    |  Space  |    | Up | Down |  Left  |   Right   |
  * `----------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12_1x2uC(
       KC_ESC  , KC_Q   , KC_W , KC_E   , KC_R , KC_T   , KC_Y  , KC_U   , KC_I    , KC_O    , KC_P    , KC_BSPC,
-      KC_TAB  , KC_A   , KC_S , KC_D   , KC_F , KC_G   , KC_H  , KC_J   , KC_K    , KC_L    , KC_SCLN , KC_ENT,
-      KC_LSFT , KC_Z   , KC_X , KC_C   , KC_V , KC_B   , KC_N  , KC_M   , KC_COMM , KC_DOT  , KC_SLSH , KC_DEL,
-      KC_LCTL , KC_LGUI, KC_NO, KC_LALT, KC_NO, LT(2,KC_SPC) , MO(1) , KC_UP  , KC_LEFT , KC_DOWN , KC_RGHT
+      KC_TAB  , KC_A   , KC_S , KC_D   , KC_F , KC_G   , KC_H  , KC_J   , KC_K    , KC_L    , KC_SCLN , KC_DEL,
+      KC_LSFT , KC_Z   , KC_X , KC_C   , KC_V , KC_B   , KC_N  , KC_M   , KC_COMM , KC_DOT  , KC_SLSH , KC_NO,
+      KC_LCTL , KC_LGUI, MO(1), KC_LALT, KC_ENT, LT(2,KC_SPC) , KC_NO , KC_UP  , KC_DOWN , KC_LEFT , KC_RGHT
 ),
 
 /* Lower (Layer 1)
  * ,---------------------------------------------------------------------------------------------------.
  * |   '   |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |       )       |      ~ `   |
  * |------+------+------+------+------+------+------+------+------+------+---------------+-------------|
- * |       |  F1  | F2   | F3   | F4   | F5   |  F6  |   - |   +  |  {   |  }            |      Pipe   |
+ * |      |      |      |      |  F2  | F12  |  F5  |   - |   +  |  {   |            }   |      Pipe   |
  * |------+------+------+------+------+------+------+------+------+------+---------------+-------------|
- * |LShift|  F7  |  F8  |  F9  | F10  | F11  | F12  |S(NUHS)|S(NUBS)|Home|     End       |             |
+ * |LShift|    |    |    |   |   |   |S(NUHS)|S(NUBS)|Home|       End     |                            |
  * |------+------+------+------+------+------+------+------+------+------+---------------+-------------|
  * |                                                                     /      Ç        /      Ã      |
  * `---------------------------------------------------------------------------------------------------'
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_ortho_4x12_1x2uC(
 
       KC_QUOT , KC_EXLM , KC_AT   , KC_HASH , KC_DLR , KC_PERC , KC_CIRC , KC_AMPR    , KC_ASTR    , KC_LPRN , KC_RPRN , KC_GRV,
-      KC_NO   , KC_F1   , KC_F2   , KC_F3   , KC_F4  , KC_F5   , KC_F6   , KC_UNDS    , KC_PLUS    , KC_LCBR , KC_RCBR , KC_PIPE,
-      KC_LSFT , KC_F7   , KC_F8   , KC_F9   , KC_F10 , KC_F11  , KC_F12  , S(KC_NUHS) , S(KC_NUBS) , KC_HOME , KC_END  , KC_NO,
+      KC_NO   , KC_NO   , KC_NO   , KC_F2   , KC_F12 , KC_F5   , KC_NO   , KC_UNDS    , KC_PLUS    , KC_LCBR , KC_RCBR , KC_PIPE,
+      KC_LSFT , KC_NO   , KC_NO   , KC_NO   , KC_NO , KC_NO  , KC_NO  , S(KC_NUHS) , S(KC_NUBS) , KC_HOME , KC_END  , KC_NO,
       KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO  , MO(3)   , KC_NO   , KC_NO      , KC_NO      , CEDILHA , AO
 
 ),
@@ -128,7 +128,8 @@ void rgb_matrix_indicators_user(void) {
                         break;
                 }
             }
-            rgb_matrix_set_color(42, 255, 33, 0);  //Layer 1 button
+            rgb_matrix_set_color(38, 255, 33, 0);  //Layer 1 button
+            rgb_matrix_set_color(40, 255, 33, 0);  //Layer 1 button
 
         break;
 
