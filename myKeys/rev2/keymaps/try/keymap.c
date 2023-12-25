6171576e36798b3c4e6e4e7bbfe4903afd939672
Shander Bonorino Andrade <s.hander8910@hotmail.com>
2023-12-25 02:31:24 -0300

    Added backspace to thumb button

--- myKeys/rev2/keymaps/try/keymap.c
+++ myKeys/rev2/keymaps/try/keymap.c
@@ -48,10 +48,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12_1x2uC(
-      KC_ESC  , KC_Q   , KC_W , KC_E   , KC_R , KC_T   , KC_Y  , KC_U   , KC_I    , KC_O    , KC_P    , KC_BSPC,
-      KC_TAB  , KC_A   , KC_S , KC_D   , KC_F , KC_G   , KC_H  , KC_J   , KC_K    , KC_L    , KC_SCLN , KC_ENT,
-      KC_LSFT , KC_Z   , KC_X , KC_C   , KC_V , KC_B   , KC_N  , KC_M   , KC_COMM , KC_DOT  , KC_SLSH , KC_DEL,
-      KC_LCTL , KC_LGUI  , TG(4), TG(3) , LALT_T(KC_ENT), LT(2,KC_SPC) , MO(1) , KC_UP  , KC_DOWN , KC_LEFT , KC_RGHT
+      KC_ESC  , KC_Q     , KC_W , KC_E   , KC_R          , KC_T         , KC_Y           , KC_U   , KC_I    , KC_O    , KC_P    , KC_BSPC,
+      KC_TAB  , KC_A     , KC_S , KC_D   , KC_F          , KC_G         , KC_H           , KC_J   , KC_K    , KC_L    , KC_SCLN , KC_ENT,
+      KC_LSFT , KC_Z     , KC_X , KC_C   , KC_V          , KC_B         , KC_N           , KC_M   , KC_COMM , KC_DOT  , KC_SLSH , KC_DEL,
+      KC_LCTL , KC_LGUI  , TG(4), TG(3)  , LALT_T(KC_ENT), LT(2,KC_SPC) , LT(1, KC_BSPC) , KC_UP  , KC_DOWN , KC_LEFT , KC_RGHT
 ),
 
 /* Lower (Layer 1)
